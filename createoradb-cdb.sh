beceb05f7dd5eb18b297a9c9aed3fec13b4cfb47
gowrishdba <gowrishdba@yahoo.com>
2015-03-16 16:08:04 +0530

    Update and rename createoradb.sh to createoradb-cdb.sh

---
+++ createoradb-cdb.sh
@@ -0,0 +1,44 @@
+#!/bin/ksh
+#--------------------------------------------------------------------------------
+#
+# createdb12c.sh
+# Gowrish M
+# Sep 2014
+#
+# to create oracle 12c rac database
+#
+#--------------------------------------------------------------------------------
+
+echo INFO - starting `date`
+echo
+
+#--------------------------------------------------------
+# source in subroutines
+echo INFO - sourcing subroutines
+. subroutines12c.sh
+
+#--------------------------------------------------------
+# call environment subroutine
+echo INFO - calling set_env to set environment
+set_env
+
+echo INFO - environment variables listing:
+env
+
+echo
+
+#exit 101
+
+#--------------------------------------------------------
+# run dbca
+echo INFO - Starting dbca
+dbcacommand="dbca -createPluggableDatabase -sourceDB $DB_UNIQUE_NAME -pdbName P2 -pdbAdminPassword lhf$DB_NAME
+
+echo
+echo $dbcacommand
+
+echo
+echo INFO - Running dbca command
+$dbcacommand
+
+echo INFO - Ending dbca
