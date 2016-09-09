598951a14673c98b97c4b1ac3710424cc1ad8001
Coder <coderdba@users.noreply.github.com>
2016-09-09 12:52:50 +0530

    Update stby-rman-dup.sh

--- dataguard11g-scripts/stby-rman-dup.sh
+++ dataguard11g-scripts/stby-rman-dup.sh
@@ -1,1 +1,1 @@
-nohup rman target sys/syspassword@primCluster-vip1/PRIM_DB_UNIQUE_NAME auxiliary sys/syspassword@stbyCluster-vip1/STBY_DB_UNIQUE_NAME @stby_rman_duplicate.cmd &
+nohup rman target sys/syspassword@primCluster-vip1/PRIM_DB_UNIQUE_NAME auxiliary sys/syspassword@stbyCluster-vip1/STBY_DB_UNIQUE_NAME @stby_rman_dup.cmd &
